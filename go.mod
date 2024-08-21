2efd9c002d70f05d11fa9eb0a2f994de9a661d1f
renoinn <oomori.lunch@gmail.com>
2024-08-21 14:29:28 +0900

    add otel

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/renoinn/kawaguchi-child-data
 
 go 1.22.4
+
+require (
+	go.opentelemetry.io/otel v1.28.0
+	go.opentelemetry.io/otel/trace v1.28.0
+)
+
+require (
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/otel/metric v1.28.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"net/http"
 	"os"
@@ -8,11 +9,22 @@ import (
 	"github.com/renoinn/kawaguchi-child-data/datasource"
 	"github.com/renoinn/kawaguchi-child-data/handler"
 	"github.com/renoinn/kawaguchi-child-data/repository"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
+
+var tracer = otel.Tracer("github.com/renoinn/kawaguchi-child-data")
 
 func main() {
 	l := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	slog.SetDefault(l)
+
+	_, span := tracer.Start(context.Background(), "Server start", trace.WithAttributes(
+		attribute.String("foo", "foo"),
+		attribute.String("hoge", "hoge"),
+	))
+	defer span.End()
 
 	d := datasource.PreschoolCsv{}
 	r := repository.NewPreschoolRepository(d)
